3e2788cfee9cedc2ae84d949322b7b08763b7ca8
AugustJing <43127428+AugustJing@users.noreply.github.com>
2020-06-17 10:40:30 -0400

    Create UserService.java

---
+++ user/src/main/java/com/ecommerce/user/service/UserService.java
@@ -0,0 +1,10 @@
+package com.ecommerce.user.service;
+
+import com.ecommerce.user.dto.UserDto;
+
+public interface UserService {
+	UserDto getUserById(Integer userId);
+	UserDto saveUser(UserDto userDto) throws Exception;
+	UserDto updateUser(UserDto userDto) throws Exception;
+	boolean loginUser(UserDto userDto) throws Exception;
+}
